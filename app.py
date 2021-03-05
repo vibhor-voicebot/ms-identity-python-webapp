8ac8b2e34ab1b1f293f041e0e02453834590aef2
vibhor-voicebot <53684148+vibhor-voicebot@users.noreply.github.com>
2021-03-06 04:59:16 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,6 +6,42 @@ import msal
 import app_config
 
 
+
+from flask import Flask, render_template, request
+from flask import Flask, jsonify, request, render_template
+import socket, os, json, yaml, sys
+import requests
+import random, os
+from azure.common.client_factory import get_client_from_cli_profile
+from azure.mgmt.resource import ResourceManagementClient
+from azure.mgmt.web import WebSiteManagementClient
+import subprocess
+from flask import Flask, render_template, request, redirect, url_for, session
+from flask_mysqldb import MySQL
+import MySQLdb.cursors
+import re
+import mysql
+import mysql.connector
+import sys, getopt
+import pymysql
+import subprocess
+from subprocess import PIPE, run
+import sys,os
+from azure.cli.core import get_default_cli
+from flask import Flask, jsonify, request, render_template
+import socket, os, json, yaml, sys
+import requests
+import random, os
+from azure.common.client_factory import get_client_from_cli_profile
+from azure.mgmt.resource import ResourceManagementClient
+from azure.mgmt.web import WebSiteManagementClient
+import subprocess
+
+#Use the get_client_from_auth_file method to create the client object:
+from azure.common.client_factory import get_client_from_auth_file
+from azure.mgmt.resource import SubscriptionClient
+
+
 app = Flask(__name__)
 app.config.from_object(app_config)
 Session(app)
@@ -61,6 +97,17 @@ def graphcall():
         headers={'Authorization': 'Bearer ' + token['access_token']},
         ).json()
     return render_template('display.html', result=graph_data)
+
+
+@app.route("/azlogin")
+def azlogin():
+    cmd = "az login"
+    loginJSON = subprocess.Popen(["bash", "-c", cmd],  stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
+    tmpOut = ""
+    tmpOut = loginJSON.stdout.read().strip().decode("utf-8")
+    print (tmpOut)
+    return str(tmpOut)
+
 
 
 def _load_cache():
